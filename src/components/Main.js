37fd5eb4b1051125fb68ba7f69c43625b2f5987d
Kanchan Bharti <kanchanbharti.6300@gmail.com>
2021-02-02 23:32:23 +0530

    Completed Draft - 1

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
-import {Switch, Route, Redirect, withRouter} from 'react-router-dom';
 
 class Main extends React.Component {
     render() {
